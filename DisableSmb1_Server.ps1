5a79ab79baf0f191d351b7f7f49d31581a5e806e
d0ugl <104329590+d0ugl@users.noreply.github.com>
2024-01-20 13:05:40 -0600

    Update DisableSmb1_Server.ps1

--- DisableSmb1_Server.ps1
+++ DisableSmb1_Server.ps1
@@ -3,3 +3,19 @@ Set-SmbServerConfiguration -EnableSMB1Protocol $false
 
 # Disable SMBv1 server on reboot
 Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol-Server -NoRestart
+
+# Verify status of SMBv1 Server Protocol
+$SMB1Status = Get-SmbServerConfiguration | Select-Object -ExpandProperty EnableSMB1Protocol
+if (-not $SMB1Status) {
+    Write-Host "SMBv1 Server Protocol is disabled."
+} else {
+    Write-Host "SMBv1 Server Protocol is still enabled."
+}
+
+# Verify status of SMBv1 Server Feature
+$SMB1Feature = Get-WindowsFeature | Where-Object { $_.Name -eq "FS-SMB1" } | Select-Object -ExpandProperty InstallState
+if ($SMB1Feature -eq "Removed") {
+    Write-Host "SMBv1 Server Feature is disabled."
+} else {
+    Write-Host "SMBv1 Server Feature is still enabled."
+}
