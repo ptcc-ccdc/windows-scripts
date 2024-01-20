bb9504d0b9d70ec7dc78905061cd9417612a461f
d0ugl <104329590+d0ugl@users.noreply.github.com>
2024-01-20 13:48:44 -0600

    Update DisableSmb1.ps1

--- DisableSmb1.ps1
+++ DisableSmb1.ps1
@@ -1,8 +1,8 @@
 # Disable SMBv1 Server
-Set-SmbServerConfiguration -EnabbleSMB1Protocol $false
+Set-SmbServerConfiguration -EnableSMB1Protocol $false
 
 # Disable SMBv1 Client
-Disable-WindowsOptionalFeature -Online -FeatureName SMBProtocol -NoRestart
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart
 
 # Verify SMBv1 Disabled
 Get-SmbServerConfiguration | Select EnableSMB1Protocol
