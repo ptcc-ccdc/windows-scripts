252d1b0615f1419d63a5ef4b0a7b45539cb07990
Notility <92754833+Notility@users.noreply.github.com>
2024-01-28 23:53:04 -0600

    This doesnt leave a bit hole open like the last 1

--- 2016/Step3ShutitDown.ps1
+++ 2016/Step3ShutitDown.ps1
@@ -1,4 +1,12 @@
-# Get all active firewall rules and disable them
+# Get all active firewall rules and disable them except for ICMPv4 Echo Request
 Get-NetFirewallRule -Enabled True | ForEach-Object {
-    $_ | Disable-NetFirewallRule
+    if ($_.DisplayName -ne "File and Printer Sharing (Echo Request - ICMPv4-In)") {
+        $_ | Disable-NetFirewallRule
+    }
+}
+
+# Enable the ICMPv4 Echo Request rule if it's disabled
+$icmpRule = Get-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)"
+if ($icmpRule.Enabled -eq $false) {
+    $icmpRule | Enable-NetFirewallRule
 }
