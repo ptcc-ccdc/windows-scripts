0a7dc74c255bf5a2d9ceb6622856ef6ac1f88f16
Notility <92754833+Notility@users.noreply.github.com>
2024-01-29 12:17:55 -0600

    Adding comments is fun

--- 2016/Step3ShutitDown.ps1
+++ 2016/Step3ShutitDown.ps1
@@ -1,4 +1,4 @@
-# Get all active firewall rules and disable them except for ICMPv4 Echo Request
+# Get all active firewall rules and disable them except for ICMPv4 Echo Request. Doing this so that the firewall understands that nothing else should be allowed onto the server
 Get-NetFirewallRule -Enabled True | ForEach-Object {
     if ($_.DisplayName -ne "File and Printer Sharing (Echo Request - ICMPv4-In)") {
         $_ | Disable-NetFirewallRule
