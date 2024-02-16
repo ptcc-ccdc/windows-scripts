2e15a8c19166ac1e26276285e03899f3d0bc998f
Notility <92754833+Notility@users.noreply.github.com>
2024-02-15 22:55:38 -0600

    Oh boy oh boy

--- 2016/Alloneprogram.ps1
+++ 2016/Alloneprogram.ps1
@@ -83,6 +83,14 @@ Write-Host "Enabling forced User Account Control (UAC)..."
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 5
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value 1
 
+#Disable NetBIOS by updating Registry
+#http://blog.dbsnet.fr/disable-netbios-with-powershell#:~:text=Disabling%20NetBIOS%20over%20TCP%2FIP,connection%2C%20then%20set%20NetbiosOptions%20%3D%202
+$key = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
+Get-ChildItem $key | ForEach-Object { 
+    Write-Host("Modify $key\$($_.pschildname)")
+    $NetbiosOptions_Value = (Get-ItemProperty "$key\$($_.pschildname)").NetbiosOptions
+    Write-Host("NetbiosOptions updated value is $NetbiosOptions_Value")
+}
 
 
 # ------------------------------------------------------------
